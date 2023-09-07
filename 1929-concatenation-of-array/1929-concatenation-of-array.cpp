981b8c463473d5844d249e18bb2af96e937ab808
Vedant Kulkarni <86275874+Vedantkulkarni21@users.noreply.github.com>
2023-09-07 23:05:03 +0530

    Update 1929-concatenation-of-array.cpp

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);cin.tie(NULL);int size=nums.size();
-        for(int i=0;i<size;i++){nums.push_back(nums[i]);}return nums;}};
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int size=nums.size();
+        for(int i=0;i<size;i++)
+        {
+            nums.push_back(nums[i]);
+        }return nums;
+    }
+};
