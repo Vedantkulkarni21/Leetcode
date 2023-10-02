db98a5c7c03ece341c7aae0432e27f475e058a9c
Vedant Kulkarni <86275874+Vedantkulkarni21@users.noreply.github.com>
2023-10-02 15:32:36 +0530

    Time: 5 ms (54.86%), Space: 9.3 MB (26.42%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string>sta;
+        int i=0;
+        for(int i=0;i<path.size();i++)
+        {
+            if(path[i]=='/')
+            {
+                continue;
+            }
+            string copy="";
+            while(path[i]!='/' && i<path.size())
+            {
+                copy+=path[i];
+                ++i;
+            }
+            if(copy==".")
+            {
+                continue;
+            }
+
+            else if(copy=="..")
+            {
+                if(!sta.empty())
+                {
+                    sta.pop();
+                }
+            }
+            else
+            {
+                sta.push(copy);
+            }
+        }
+        string str="";
+        while(!sta.empty())
+        {
+            str="/"+ sta.top() +str;
+            sta.pop();
+        }
+        if(str.size()==0)
+        {
+            str.push_back('/');
+        }
+        return str;
+    }
+};
