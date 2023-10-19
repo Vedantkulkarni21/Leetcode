427cdd5d4b28458baad1c89a773d6ed2b323cc64
Vedant Kulkarni <86275874+Vedantkulkarni21@users.noreply.github.com>
2023-10-19 16:41:41 +0530

    Time: 3 ms (47.11%), Space: 6.7 MB (39.46%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,52 +1,90 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char>sta,sta1;
         string copy1,copy2;
         for(char ch:s)
         {
-            if(sta.empty() && ch=='#')
+            if(copy1.size()==0 && ch=='#')
             {
                 continue;
             }
             else if(ch=='#')
             {
-                sta.pop();
+                copy1.pop_back();
             }
-            else if(ch != '#')
+            else if(ch!='#')
             {
-                sta.push(ch);
+                copy1.push_back(ch);
             }
         }
+
         for(char ch:t)
         {
-            if(sta1.empty() && ch=='#')
+            if(copy2.size()==0 && ch=='#')
             {
                 continue;
             }
             else if(ch=='#')
             {
-                sta1.pop();
+                copy2.pop_back();
             }
-            else if(ch != '#')
+            else if(ch!='#')
             {
-                sta1.push(ch);
+                copy2.push_back(ch);
+                cout<<ch;
             }
         }
-        while(!sta.empty())
-        {
-            copy1+=sta.top();
-            sta.pop();
-        }
-        while(!sta1.empty())
-        {
-            copy2+=sta1.top();
-            sta1.pop();
-        }
-        if(copy1==copy2)
-        {
-            return true;
-        }
+
+        if(copy1!=copy2)
         return false;
+
+        return true;
+        // stack<char>sta,sta1;
+        // string copy1,copy2;
+        // for(char ch:s)
+        // {
+        //     if(sta.empty() && ch=='#')
+        //     {
+        //         continue;
+        //     }
+        //     else if(ch=='#')
+        //     {
+        //         sta.pop();
+        //     }
+        //     else if(ch != '#')
+        //     {
+        //         sta.push(ch);
+        //     }
+        // }
+        // for(char ch:t)
+        // {
+        //     if(sta1.empty() && ch=='#')
+        //     {
+        //         continue;
+        //     }
+        //     else if(ch=='#')
+        //     {
+        //         sta1.pop();
+        //     }
+        //     else if(ch != '#')
+        //     {
+        //         sta1.push(ch);
+        //     }
+        // }
+        // while(!sta.empty())
+        // {
+        //     copy1+=sta.top();
+        //     sta.pop();
+        // }
+        // while(!sta1.empty())
+        // {
+        //     copy2+=sta1.top();
+        //     sta1.pop();
+        // }
+        // if(copy1==copy2)
+        // {
+        //     return true;
+        // }
+        // return false;
     }
 };
