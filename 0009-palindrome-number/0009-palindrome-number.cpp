aaf5f31b9e628c73b91fa4f604ced22908a3a4ba
Vedant Kulkarni <86275874+Vedantkulkarni21@users.noreply.github.com>
2024-06-13 14:45:17 +0530

    Time: 0 ms (100%), Space: 8.2 MB (78.86%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        int rem=0,temp = x;
+        long ans=0;
         if(x<0)
-        {return false;}
-        int temp=x;
-        long int rem=0,sum=0;
+            return false;
         while(x!=0)
-        {
-            rem=x%10;
-             sum=sum*10+rem;
-            x=x/10;
+        { 
+            rem = x%10;
+            ans = ans*10+rem;
+            x = x/10;
         }
-        cout<<sum;
-        if(sum==temp)
-        {
+        if(ans == temp)
             return true;
-        }
         return false;
     }
 };
