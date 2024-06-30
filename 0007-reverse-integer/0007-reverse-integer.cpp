19266313be6891e043dbc078189f00e8711be029
Vedant Kulkarni <86275874+Vedantkulkarni21@users.noreply.github.com>
2024-07-01 01:59:25 +0530

    Time: 5 ms (21.12%), Space: 7.3 MB (87.03%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int reverse(int x) {
         int rhs=0,ans=0;
-        cout<<x%10<<endl;
-        cout<<INT_MAX;
         while(x!=0)
         {
             if(ans > INT_MAX/10 || ans < INT_MIN/10)
