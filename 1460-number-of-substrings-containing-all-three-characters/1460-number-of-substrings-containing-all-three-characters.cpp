43a687a88e1fabd15601a5a2eb52bba1f2c274fe
Vedant Kulkarni <86275874+Vedantkulkarni21@users.noreply.github.com>
2024-07-11 08:41:41 +0530

    Time: 66 ms (9.44%), Space: 10.9 MB (28.92%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -4,16 +4,16 @@ public:
         int l = 0, r = 0, count = 0;
         map<char, int> mpp;
 
-        for (r = 0; r < s.size(); r++) {
+        for(;r<s.size();r++)
+        {
             mpp[s[r]]++;
-
-            while (mpp['a'] > 0 && mpp['b'] > 0 && mpp['c'] > 0) {
-                count += s.size() - r;
+            while(mpp['a'] > 0 && mpp['b'] > 0 && mpp['c'] > 0)
+            {
+                count+=s.size()-r;
                 mpp[s[l]]--;
                 l++;
             }
         }
-
         return count;
     }
 };
