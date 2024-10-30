57aaaae93ab7540017887268c6dc90207cc172ba
Vedant Kulkarni <86275874+Vedantkulkarni21@users.noreply.github.com>
2024-10-30 19:42:51 +0530

    Time: 63 ms (42.68%), Space: 81.2 MB (57.97%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        map<int, int> mpp;
-        for (int i = 0; i < nums.size(); i++) {
-            if (mpp.find(nums[i]) != mpp.end() && abs(i - mpp[nums[i]]) <= k) {
-                return true;  // Found a nearby duplicate
+        unordered_map<int,int>mpp;
+        int l=0,r=0;
+        while(r<nums.size())
+        {
+            mpp[nums[r]]++;
+            if(r-l > k)
+            {
+                mpp[nums[l]]--;
+                l++;
             }
-            mpp[nums[i]] = i;
+            if(mpp[nums[r]]>1)
+                return true;
+            r++;
         }
-        return false;  // No nearby duplicates found
+        return false;
     }
 };
-
-
-
-// class Solution {
-// public:
-//     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-//         map<int,int>mpp;
-//         for(int i=0;i<nums.size();i++)
-//         {
-//             if((nums[i]==nums[mpp[nums[i]]]) && (mpp[nums[i]]!=0) && abs(i-mpp[nums[i]])<=k)
-//             {
-//                 cout<<nums[mpp[nums[i]]]<<endl;
-//                 return true;
-//             }
-            
-//             mpp[nums[i]]=i;
-//         }
-//         for(auto it:mpp)
-//         {
-//             cout<<it.first<<" "<<it.second<<endl;
-//         }
-//         return false;
-//     }
-// };
