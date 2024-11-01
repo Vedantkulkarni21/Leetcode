58f430a96074ae57d314de3479b59cf1ed4e24a6
Vedant Kulkarni <86275874+Vedantkulkarni21@users.noreply.github.com>
2024-11-01 14:50:09 +0530

    Time: 0 ms (100%), Space: 18.3 MB (63.28%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,27 +1,51 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& numbers, int target) {
+//         int n=0;
+//         while(n<numbers.size())
+//         {
+//             int l=n+1,r=numbers.size()-1,mid=0;
+//             while(l<=r)
+//             {
+//                 mid = l+(r-l)/2;
+//                 if(numbers[mid] == (target-numbers[n]))
+//                 {
+//                     return {n+1,mid+1};
+//                 } 
+//                 else if(numbers[mid] > (target - numbers[n]))
+//                 {
+//                     r=mid-1;
+//                 }
+//                 else
+//                 {
+//                     l=mid+1;
+//                 }
+//             }
+//             n++;
+//         }
+//         return {};
+//     }
+// };
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int n=0;
-        while(n<numbers.size())
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        int l=0,r=nums.size()-1;
+        while(l<r)
         {
-            int l=n+1,r=numbers.size()-1,mid=0;
-            while(l<=r)
+            if(nums[l]+nums[r] > target)
             {
-                mid = l+(r-l)/2;
-                if(numbers[mid] == (target-numbers[n]))
-                {
-                    return {n+1,mid+1};
-                } 
-                else if(numbers[mid] > (target - numbers[n]))
-                {
-                    r=mid-1;
-                }
-                else
-                {
-                    l=mid+1;
-                }
+                r--;
             }
-            n++;
+            else if(nums[l]+nums[r] < target)
+            {
+                l++;
+            }
+            else
+            {
+                return {l+1 ,r+1};
+            }
         }
         return {};
     }
