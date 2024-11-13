9f70042664fb4fd823828c3c2a24edae1d418caa
Vedant Kulkarni <86275874+Vedantkulkarni21@users.noreply.github.com>
2024-11-13 20:11:25 +0530

    Time: 157 ms (32.98%), Space: 111.5 MB (20.13%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -1,51 +1,25 @@
-// class Solution {
-// public:
-//     int longestSquareStreak(vector<int>& nums) {
-//         map<int,int>mpp;
-//         int count=0,maxi = 0;
-//         for(int it:nums)
-//         {
-//             mpp[it]++;
-//         }
-//         for(int i=0;i<nums.size();i++)
-//         {
-//             int num = nums[i];
-//             while(mpp.find(num*num) != mpp.end())
-//             {
-//                 count++;
-//                 num = num*num;
-//             }
-//             maxi = max(count,maxi);
-//             count=0;
-//         }
-//         if(maxi == 0)
-//             return -1;
-//         return maxi+1;
-//     }
-// };
-
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        map<int, int> mpp;
-        int maxi = 0;
-
-        for (int it : nums) {
+        map<int,int>mpp;
+        int count=0,maxi = 0;
+        for(int it:nums)
+        {
             mpp[it]++;
         }
-
-        for (int i = 0; i < nums.size(); i++) {
-            int count = 0;
+        for(int i=0;i<nums.size();i++)
+        {
             int num = nums[i];
-            
-            while (num <= INT_MAX / num && mpp.find(num * num) != mpp.end()) {
+            while(num <= INT_MAX/num && mpp.find(num*num) != mpp.end())
+            {
                 count++;
-                num = num * num;
+                num = num*num;
             }
-
-            maxi = max(count, maxi);
+            maxi = max(count,maxi);
+            count=0;
         }
-
-        return maxi == 0 ? -1 : maxi + 1;
+        if(maxi == 0)
+            return -1;
+        return maxi+1;
     }
 };
